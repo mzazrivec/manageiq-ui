ManageIQ/manageiq

Menu redesign: add section icons and detect subsections.

app/presenters/menu/custom_loader.rb
@@ -42,11 +42,12 @@ module Menu
     end
 
     def create_custom_menu_section(properties)
+      before       = properties.key?('icon') ? properties['icon'] : nil
       placement    = properties.key?('placement') ? properties['placement'].to_sym : :default
       before       = properties.key?('before') ? properties['before'].to_sym : nil
       section_type = properties.key?('section_type') ? properties['section_type'].to_sym : :default
       href         = properties.key?('href') ? properties['href'].to_sym : nil
-      Section.new(properties['id'].to_sym, properties['name'], [], placement, before, section_type, href)
+      Section.new(properties['id'].to_sym, properties['name'], icon, [], placement, before, section_type, href)
     end
   end
 end

mzazrivec/manageiq-ui

app/presenters/menu/custom_loader.rb
@@ -42,11 +42,12 @@ module Menu
     end
 
     def create_custom_menu_section(properties)
+      icon         = properties.key?('icon') ? properties['icon'] : nil
       placement    = properties.key?('placement') ? properties['placement'].to_sym : :default
       before       = properties.key?('before') ? properties['before'].to_sym : nil
       section_type = properties.key?('section_type') ? properties['section_type'].to_sym : :default
       href         = properties.key?('href') ? properties['href'].to_sym : nil
-      Section.new(properties['id'].to_sym, properties['name'], [], placement, before, section_type, href)
+      Section.new(properties['id'].to_sym, properties['name'], icon, [], placement, before, section_type, href)
     end
   end
 end