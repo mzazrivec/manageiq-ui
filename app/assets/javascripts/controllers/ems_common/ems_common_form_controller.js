AparnaKarve/manageiq

Fix validation rules taking into account different provider types

app/assets/javascripts/controllers/ems_common/ems_common_form_controller.js
@@ -128,7 +128,12 @@ ManageIQ.angular.app.controller('emsCommonFormController', ['$http', '$scope', '
 
   $scope.isBasicInfoValid = function() {
     if(($scope.currentTab == "default" && $scope.emsCommonModel.emstype != "azure") &&
-      ($scope.emsCommonModel.emstype == "ec2" || ($scope.emsCommonModel.emstype == "openstack" && $scope.emsCommonModel.hostname)) &&
+      ($scope.emsCommonModel.emstype == "ec2" ||
+      ($scope.emsCommonModel.emstype == "openstack" && $scope.emsCommonModel.hostname ||
+       $scope.emsCommonModel.emstype == "scvmm" && $scope.emsCommonModel.hostname ||
+       $scope.emsCommonModel.emstype == "openstack_infra" && $scope.emsCommonModel.hostname ||
+       $scope.emsCommonModel.emstype == "rhevm" && $scope.emsCommonModel.hostname && $scope.emsCommonModel.api_port ||
+       $scope.emsCommonModel.emstype == "vmwarews" && $scope.emsCommonModel.hostname)) &&
       ($scope.emsCommonModel.default_userid != '' && $scope.angularForm.default_userid.$valid &&
        $scope.emsCommonModel.default_password != '' && $scope.angularForm.default_password.$valid &&
        $scope.emsCommonModel.default_verify != '' && $scope.angularForm.default_verify.$valid)) {
@@ -151,11 +156,18 @@ ManageIQ.angular.app.controller('emsCommonFormController', ['$http', '$scope', '
       $scope.emsCommonModel.ssh_keypair_password != '' && $scope.angularForm.ssh_keypair_password.$valid &&
       $scope.emsCommonModel.ssh_keypair_verify != '' && $scope.angularForm.ssh_keypair_verify.$valid)) {
       return true;
+    } else if(($scope.currentTab == "metrics" && $scope.emsCommonModel.emstype == "rhevm") &&
+      ($scope.emsCommonModel.hostname != '' && $scope.angularForm.hostname.$valid && $scope.emsCommonModel.api_port != '') &&
+      ($scope.emsCommonModel.metrics_userid != '' && $scope.angularForm.metrics_userid.$valid &&
+      $scope.emsCommonModel.metrics_password != '' && $scope.angularForm.metrics_password.$valid &&
+      $scope.emsCommonModel.metrics_verify != '' && $scope.angularForm.metrics_verify.$valid)) {
+      return true;
     } else if($scope.emsCommonModel.emstype == "gce" && $scope.emsCommonModel.project != '' &&
       ($scope.currentTab == "default" || 
       ($scope.currentTab == "service_account" && $scope.emsCommonModel.service_account != ''))) {
       return true;
     }
+
     else
       return false;
   };

mzazrivec/manageiq-ui

app/assets/javascripts/controllers/ems_common/ems_common_form_controller.js
@@ -140,7 +140,12 @@ ManageIQ.angular.app.controller('emsCommonFormController', ['$http', '$scope', '
 
   $scope.isBasicInfoValid = function() {
     if(($scope.currentTab == "default" && $scope.emsCommonModel.emstype != "azure") &&
-      ($scope.emsCommonModel.emstype == "ec2" || ($scope.emsCommonModel.emstype == "openstack" && $scope.emsCommonModel.default_hostname)) &&
+      ($scope.emsCommonModel.emstype == "ec2" ||
+      ($scope.emsCommonModel.emstype == "openstack" && $scope.emsCommonModel.default_hostname ||
+       $scope.emsCommonModel.emstype == "scvmm" && $scope.emsCommonModel.hostname ||
+       $scope.emsCommonModel.emstype == "openstack_infra" && $scope.emsCommonModel.hostname ||
+       $scope.emsCommonModel.emstype == "rhevm" && $scope.emsCommonModel.hostname && $scope.emsCommonModel.api_port ||
+       $scope.emsCommonModel.emstype == "vmwarews" && $scope.emsCommonModel.hostname)) &&
       ($scope.emsCommonModel.default_userid != '' && $scope.angularForm.default_userid.$valid &&
        $scope.emsCommonModel.default_password != '' && $scope.angularForm.default_password.$valid &&
        $scope.emsCommonModel.default_verify != '' && $scope.angularForm.default_verify.$valid)) {
@@ -163,11 +168,18 @@ ManageIQ.angular.app.controller('emsCommonFormController', ['$http', '$scope', '
       $scope.emsCommonModel.ssh_keypair_password != '' && $scope.angularForm.ssh_keypair_password.$valid &&
       $scope.emsCommonModel.ssh_keypair_verify != '' && $scope.angularForm.ssh_keypair_verify.$valid)) {
       return true;
+    } else if(($scope.currentTab == "metrics" && $scope.emsCommonModel.emstype == "rhevm") &&
+      ($scope.emsCommonModel.hostname != '' && $scope.angularForm.hostname.$valid && $scope.emsCommonModel.api_port != '') &&
+      ($scope.emsCommonModel.metrics_userid != '' && $scope.angularForm.metrics_userid.$valid &&
+      $scope.emsCommonModel.metrics_password != '' && $scope.angularForm.metrics_password.$valid &&
+      $scope.emsCommonModel.metrics_verify != '' && $scope.angularForm.metrics_verify.$valid)) {
+      return true;
     } else if($scope.emsCommonModel.emstype == "gce" && $scope.emsCommonModel.project != '' &&
       ($scope.currentTab == "default" ||
       ($scope.currentTab == "service_account" && $scope.emsCommonModel.service_account != ''))) {
       return true;
     }
+
     else
       return false;
   };